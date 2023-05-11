a013976cd2e3b3cb15c1dc1b9c0c8131d165ce18
Raveena Rathnam <D00251856@student.dkit.ie>
2023-05-11 02:54:52 +0100

    updated the index.blade.php
    added the recent post section.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -178,59 +178,53 @@ As someone who's walked in your shoes, I'm arming you with stunning photos, expe
 </div>
 
 
-<div class="text-center p-15 bg-black text-white">
-    <h2 class="text-2xl pb-5 text-l"> 
-        I'm an expert in...
+<div class="text-center p-15 bg-purple-300 text-white">
+    <h2 class="text-3xl text-gray-600 pb-5 text-l"> 
+        "I have a genuine passion...
     </h2>
 
-    <span class="font-extrabold block text-4xl py-1">
-        Ux Design
+    <span class="font-extrabold block text-gray-600 text-4xl py-1">
+        for travel,
     </span>
-    <span class="font-extrabold block text-4xl py-1">
-        Project Management
+    <span class="font-extrabold block text-gray-600 text-4xl py-1">
+        and my engaging storytelling brings destinations to life,
     </span>
-    <span class="font-extrabold block text-4xl py-1">
-        Digital Strategy
+    <span class="font-extrabold block text-gray-600 text-4xl py-1">
+        inspiring readers to embark on their own adventures."
     </span>
-    <span class="font-extrabold block text-4xl py-1">
-        Backend Development
-    </span>
+     
 </div>
 
-<div class="text-center py-15">
-    <span class="uppercase text-s text-gray-400">
+<div class="text-center  py-15">
+    {{-- <span class="uppercase text-s text-gray-400">
         Blog
-    </span>
+    </span> --}}
 
     <h2 class="text-4xl font-bold py-10">
         Recent Posts
     </h2>
 
     <p class="m-auto w-4/5 text-gray-500">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque exercitationem saepe enim veritatis, eos temporibus quaerat facere consectetur qui.
-    </p>
+        "Discover the latest updates and delve into our recent post to stay informed about the newest travel experiences and destinations."
+     </p>
 </div>
 
 <div class="sm:grid grid-cols-2 w-4/5 m-auto">
-    <div class="flex bg-yellow-700 text-gray-100 pt-10">
+    <div class="flex bg-indigo-400 text-gray-100 pt-10">
         <div class="m-auto pt-4 pb-16 sm:m-auto w-4/5 block">
-            <span class="uppercase text-xs">
-                PHP
-            </span>
-
             <h3 class="text-xl font-bold py-10">
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Voluptas necessitatibus dolorum error culpa laboriosam. Enim voluptas earum repudiandae consequuntur ad? Expedita labore aspernatur facilis quasi ex? Nemo hic placeat et?
-            </h3>
-
+                "Seattle's vibrant coffee scene is a captivating blend of rich flavors, cozy cafes, and passionate baristas, making it a must-visit destination for coffee enthusiasts around the world."
+             </h3>
             <a 
-                href=""
+                href="/blog/seattle-coffee-scene"
                 class="uppercase bg-transparent border-2 border-gray-100 text-gray-100 text-xs font-extrabold py-3 px-5 rounded-3xl">
-                Find Out More
+                More Coffee?!
             </a>
         </div>
     </div>
-    <div>
-        <img src="https://cdn.pixabay.com/photo/2014/05/03/01/03/laptop-336704_960_720.jpg" alt="">
-    </div>
+    <div  >
+            <img src="/images/p3.jpg" width="700"   alt="">
+            
+          </div>
 </div> 
 @endsection
