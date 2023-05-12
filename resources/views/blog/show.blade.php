5441c05ac3e3068de85e1930cb97d095e816f703
kosystephy <kosysteve03@gmail.com>
2023-05-12 17:31:30 +0100

    added image to the showblade page

--- resources/views/blog/show.blade.php
+++ resources/views/blog/show.blade.php
@@ -3,12 +3,15 @@
 @section('content')
 <div class="w-4/5 m-auto text-left">
     <div class="py-15">
+        <div>
+            <img src="{{ asset('images/' . $post->image_path) }}" alt="">
+        </div>
         <h1 class="text-6xl">
-            {{ $post->title }}
+                      {{ $post->title }} </h1>
             <p class="text-xl text-gray-700 pt-8 pb-10 leading-8 font-light">
                 {{ $post->info }}
             </p>
-        </h1>
+
     </div>
 </div>
 
