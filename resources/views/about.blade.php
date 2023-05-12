46d075f403a849f579a8804beb2bdbca7f6bcaa8
Raveena Rathnam <D00251856@student.dkit.ie>
2023-05-12 22:41:29 +0100

    Updated the about.blade.php
    added the code for the about page.

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,7 +1,95 @@
 @extends('layouts.app')
 
 @section('content')
-    <h1>About Us</h1>
-    <p>Welcome to our travel blog. We are passionate about exploring the world and sharing our experiences with you.</p>
-    <p>Feel free to browse through our articles and get inspired for your next adventure!</p>
+ 
+<div class = "about-wrapper1">
+    <div class = "about-left">
+      <div class = "about-left-content">
+        <div>
+          <div class = "shadow">
+            <div class = "about-img">
+              <img src = "/images/profile1.png" alt = "about image">
+            </div>
+          </div>
+
+          <h2>Raveena<br>Rathnam</h2>
+          <h3></h3>
+        </div>
+
+        <ul class = "icons">
+          <li><i class = "fab fa-facebook-f"></i></li>
+          <li><i class = "fab fa-twitter"></i></li>
+          <li><i class = "fab fa-linkedin"></i></li>
+          <li><i class = "fab fa-instagram"></i></li>
+        </ul>
+      </div>
+    </div>
+
+    <div class = "about-right">
+      <h1>hi<span style="color:#FED7E2">!</span></h1>
+      <h2>Here's who I am & what I do</h2>
+      
+
+      <div class = "about-para">
+        <p>"Greetings, fellow wanderers! I'm Raveena, an adventurous soul with an insatiable curiosity for the world."</p>
+        <p>"Through the power of words, I aim to paint vivid pictures and evoke emotions that transport you to the heart of our travel experiences. From the thrill of hiking in majestic mountains to the serenity of wandering through ancient ruins, I endeavor to convey the essence of our adventures. As we embark on this incredible journey, I invite you to join me in discovering hidden gems, embracing diverse cultures, and embracing the transformative power of travel. Let's embark on a virtual expedition together!"</p>
+      </div>
+    </div>
+  </div>
+
+
+  <div class="text-center p-15  bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white">
+    <h2 class="text-3xl pb-5 text-l"> 
+        "We are two friends who set out on this journey...
+    </h2>
+
+    <span class="font-extrabold block  text-4xl py-1">
+        discovering new places,
+    </span>
+    <span class="font-extrabold block  text-4xl py-1">
+        sharing our wonderful experiences on this platform,
+    </span>
+    <span class="font-extrabold block  text-4xl py-1">
+       and inspiring readers to embark on their own adventures."
+    </span>
+     
+</div>
+
+  <div class = "about-wrapper2">
+    
+
+    <div class = "about-right">
+      <h1>hi<span style="color:#DDD6FE">!</span></h1>
+      <h2>Here's who I am & what I do</h2>
+      
+
+      <div class = "about-para">
+        <p>"Hello there! I'm Kosy, an enthusiastic traveler and a passionate photographer."</p>
+        <p>"As I explore the world, my camera is my trusted companion, capturing the awe-inspiring landscapes and the vibrant cultures I encounter along the way. Through my lens, I strive to freeze the magical moments in time and share them with all of you. Whether it's a stunning sunset over a pristine beach or the bustling energy of a bustling city, I aim to transport you to these places through my photographs. Join me on this visual journey and let's discover the beauty of our extraordinary world together!"</p>
+      </div>
+    </div>
+
+    <div class = "about-left1">
+        <div class = "about-left-content1">
+          <div>
+            <div class = "shadow">
+              <div class = "about-img">
+                <img src = "/images/profile2.png" alt = "about image">
+              </div>
+            </div>
+  
+            <h2>Kosy<br>Stephanie</h2>
+            <h3></h3>
+          </div>
+  
+          <ul class = "icons">
+            <li><i class = "fab fa-facebook-f"></i></li>
+            <li><i class = "fab fa-twitter"></i></li>
+            <li><i class = "fab fa-linkedin"></i></li>
+            <li><i class = "fab fa-instagram"></i></li>
+          </ul>
+        </div>
+      </div>
+  </div>
+ 
 @endsection
